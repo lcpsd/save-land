fddc1bc43746769e3aaa7dc04ecb875b833712b5
Lucas Cardoso <lucascardosopsd@gmail.com>
2022-02-06 14:32:07 -0300

    download link component base

--- src/App/DownloadSection/index.jsx
+++ src/App/DownloadSection/index.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
+import { DownloadLink } from './DownloadLink'
 import { InputLink } from './InputLink'
 import { Loading } from './Loading'
 import styles from './styles.module.scss'
 
 export function DownloadSection(){
 
-    const [loading, setLoading] = useState(true)
-    const [links, setLinks] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [links, setLinks] = useState("a")
 
     return(
         <section className={styles.downloadSection}>
             <div className={styles.container}>
                 {(!loading && !links) && <InputLink/>}
                 {loading && <Loading/>}
-
+                <DownloadLink/>
             </div>
         </section>
     )
